f7fc902d7654711eac807230b248c265f3272910
M-Mabrouk1 <MuhammadMabrouk@gmail.com>
2022-04-10 21:02:12 +0200

    removed neon-i theme to add it to a separate  repo

--- .config/leftwm/themes/neon-i/picom/picom-toggle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if pgrep -x "picom" > /dev/null
-then
-	killall picom; notify-send "Picom Killed"
-else
-	picom --experimental-backends -b --config "$SCRIPTPATH"/picom/picom.conf; notify-send "Picom enabled"
-fi
--- .config/leftwm/themes/neon-i/scripts/mouse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-path=$(find /sys/class/power_supply -name "hidpp*")
-if [ -z "$path" ]
-then
-      :
-else
-      capacity=$(cat $path/capacity)
-      echo "$capacity%"
-fi
